e80fc1d4a731287204443ec1d401eb8a70e9c952
HJRYU\o__hy <o__hyeah@naver.com>
2023-03-14 17:07:25 +0900

    feat: Create SEO component, set head title (#4)

---
+++ frontend/src/components/Seo.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Seo({ title }: { title: string }) {
+  return (
+    <Head>
+      <title>{title} | DEV-VALLEY</title>
+    </Head>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,9 @@
+import Seo from "@/components/Seo";
+
 export default function Home() {
   return (
     <>
+      <Seo title="Home" />
       <div>Home</div>
     </>
   );
