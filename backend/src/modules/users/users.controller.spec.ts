d68465eadbacda3ac8aed1a3d64c122dadfec620
Byungho <57972951+ho-1@users.noreply.github.com>
2023-03-23 17:28:01 +0900

    chore: 유저 폴더 수정 (#6)

--- backend/src/modules/users/users.controller.spec.ts
+++ backend/src/modules/users/users.controller.spec.ts
@@ -1,5 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { UsersController } from "./users.controller";
 
 describe('UsersController', () => {
   let controller: UsersController;
