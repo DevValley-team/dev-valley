d3897f2f33eda16e0830e2ab2d5ff2bc658f9f25
Singsoong <o__hyeah@naver.com>
2023-03-26 17:44:25 +0900

    chore: Create GlobalStyle, Apply fonts (#7)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,11 @@
+import { GlobalStyle } from "@/components/GlobalStyle";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
