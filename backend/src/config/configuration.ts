e65addbe4041a47de93a13d55417435e2a216123
Byungho <57972951+ho-1@users.noreply.github.com>
2023-04-03 00:19:37 +0900

    feat: helmet, cors 추가

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -2,6 +2,7 @@ export default () => ({
   app: {
     host: process.env.HOST || "localhost",
     port: parseInt(process.env.PORT, 10) || 3000,
+    clientUrl: process.env.CLIENT_URL || ["*", "http://localhost:3000"],
     jwtSecret: process.env.JWT_SECRET
   },
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,16 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.use(helmet());
+  app.enableCors({ origin: configService.get<string>('app.clientUrl') });
   app.setGlobalPrefix('api')
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(configService.get<number>('app.port'));
 }
 bootstrap();
