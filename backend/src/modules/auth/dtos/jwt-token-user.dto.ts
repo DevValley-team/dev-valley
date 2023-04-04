987499c92ac7fbfe2e5f5be075ef1fa34e98c329
Byungho <57972951+ho-1@users.noreply.github.com>
2023-04-04 23:09:38 +0900

    chore: 파일 위치 변경, 이름 변경

--- backend/src/modules/auth/dtos/jwt-token-user.dto.ts
+++ backend/src/modules/auth/dtos/jwt-token-user.dto.ts
@@ -1,4 +1,4 @@
-export class JwtTokenDto {
+export class JwtTokenUserDto {
   id: number;
   email: string;
   nickname: string;
--- backend/src/modules/users/dtos/user-response.dto.ts
+++ backend/src/modules/users/dtos/user-response.dto.ts
@@ -1,18 +1,24 @@
 import { Expose } from "class-transformer";
 
-export class UserDto {
+export class UserResponseDto {
   @Expose()
   id: number;
+
   @Expose()
   email: string;
+
   @Expose()
   nickname: string;
+
   @Expose()
   experience: number;
+
   @Expose()
   lastLogInAt: Date;
+
   @Expose()
   createdAt: Date;
+
   @Expose()
   updatedAt: Date;
 }
